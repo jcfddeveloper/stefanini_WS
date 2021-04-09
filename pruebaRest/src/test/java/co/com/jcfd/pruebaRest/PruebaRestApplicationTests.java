d3859ce5b0c328763eebc9b665771fc401843e3c
Julio Cesar Ferreira <jcfd.developer@gmail.com>
2021-04-09 18:03:01 -0500

    creacion de proyecto base

---
+++ pruebaRest/src/test/java/co/com/jcfd/pruebaRest/PruebaRestApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.jcfd.pruebaRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
